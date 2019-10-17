0759d42336aba6583630179b8828f01ef45632a9
MetalFever <zac.jeckell+github@gmail.com>
2019-10-17 11:38:29 -0500

    removed gatsby-plugin-google-analytics and added snippet

--- gatsby-config.js
+++ gatsby-config.js
@@ -171,12 +171,7 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID
-      }
-    },
+    
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
